14df14bfe02ec8df8315cead025f1510ca8015c6
Robert Surton <burgess@alum.rpi.edu>
2021-12-17 19:30:32 -0800

    Add template for easily starting new projects

---
+++ template/part1.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <bitset>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	return EXIT_SUCCESS;
+}
